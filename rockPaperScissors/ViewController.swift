bd6ba9e3798b4653c21e171f551c892f14dd74a0
Jeong Jae Myeong <piremi@gmail.com>
2017-09-20 14:17:39 +0900

    layout constraints are all set

--- rockPaperScissors/ViewController.swift
+++ rockPaperScissors/ViewController.swift
@@ -10,9 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var comImageView: UIImageView!
+    @IBOutlet weak var playerImageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let angle:CGFloat = CGFloat((180.0 * Double.pi)/180.0)
+        comImageView.transform = CGAffineTransform(rotationAngle: angle)
     }
 
     override func didReceiveMemoryWarning() {
